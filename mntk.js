56155e59314ac2c1a1e2fb9abf570b50b74b3a26
null8080 <mrnull8080@gmail.com>
2022-03-31 05:27:16 +0000

    Better.

--- mntk.js
+++ mntk.js
@@ -1,39 +1,39 @@
-var $X = function() {
-  var $XF = function() {
-    return this._$X._cli.apply(this._$X, arguments);
-  };
-  if (!this._$X) {
-    this._$X = {
-      author: 'MrNull.io',
-      v: 'v1.0',
-      _f: [],
-      _cli: function(a, b, c) {
-        var T = this,
-          A = arguments;
-        if (typeof a == 'function' && !b && !c) {
-          T._f.push(a);
-        } else {
-          var R;
-          T._f.forEach(function(_f) {
-            let r = _f.apply(T, A);
-            if (typeof r !== 'undefined') R = r;
-          });
-          return R;
-        }
+(function(){
+    
+    var _u='undefined',
+        _f='function',
+        _o='object',
+        _c='constructor';
+    
+    var $X = function() {
+    
+      var $T = this, $XF = function(a) { 
+           var T=this, A=arguments;
+          if (A.length == 1 && typeof a==_f) {
+              T.__o.f.push(a);
+          }
+          else {
+            var R;
+            T.__o.f.forEach(function(C) {
+              var r = C.apply(C,A);
+              if (typeof r!==_u) R=r;
+            });
+            return R;
+          }
+      };
+      $T._$X = {
+        __v: 'v0.1',
+        __o: {f:[]}
+      };
+      $XF = $XF.bind($T._$X);
+      for(var x in $T._$X) {
+        if (typeof $T._$X[x]==_f) $XF[x] = $T._$X[x].bind($T._$X);
       }
+      return $XF;
     };
-    for (var x1 in this._$X) {
-      var Xx = this._$X[x1];
-      if (typeof Xx == 'function') $XF[x1] = this._$X[x1].bind(this._$X);
-    }
-  }
-  for (var x2 in this._$X) {
-    if (typeof this._$X[x2] == 'function') this._$X[x2].bind(this._$X);
-  }
-  return $XF;
-};
-
-
-// Export
-if (typeof window == 'object') window.$X = $X;
-else if (typeof module !== 'undefined') module.exports = $X;
+    
+    var W = (typeof window!==_u?window:0), M = (typeof module!==_u?module:0);
+    if (typeof W==_o && typeof W[_c]==_f && W[_c].name=='Window') W.$X = $X;
+    else if (typeof M==_o && typeof M[_c]==_f && M[_c].name=='Module') M.exports = $X;
+    
+})();
